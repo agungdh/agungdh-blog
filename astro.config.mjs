ff9e393c166b1adac00692f4e832f53c54a771f8
= <=>
2022-08-20 17:49:47 +0700

    deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from 'astro-robots-txt';
+import compress from "astro-compress";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://agungdh.com',
+  integrations: [mdx(), sitemap(), tailwind(), compress(), robotsTxt(), svelte()]
 });
