ec4ab9ffb038dd5828a748a313eefe06563bdd74
= <=>
2022-08-20 18:04:55 +0700

    deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from 'astro-robots-txt';
 import compress from "astro-compress";
-
-import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://agungdh.com',
-  integrations: [mdx(), sitemap(), tailwind(), compress(), robotsTxt(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind(), compress(), robotsTxt(), react()]
 });
